0a163bf4703ddd4a71c3de2c02676473211eec2a
Jacob Swartwood <jswartwood@gmail.com>
2012-08-29 10:47:30 -0400

    Adding show hidden files to .osx and adjusting my setup script.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 /usr/bin/ruby -e "$(/usr/bin/curl -fsSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)"
 
 # Install tools
-brew install ack git legit lynx phantomjs subversion hg
+brew install git subversion hg legit ack lynx phantomjs
 
 # Install languages
 brew install lua android-sdk node
@@ -10,14 +10,13 @@ brew install lua android-sdk node
 # Install DBs
 brew install couchdb mongodb redis
 
-# Install NPM
-curl http://npmjs.org/install.sh | sh
-
 # Install NPM global fav tools
-npm install -g grunt coffee-script nodemon express
+npm install -g grunt coffee-script nodemon express weinre
 
 # Install Nodejistu cli
 npm install -g jitsu
 
 # Install Heroku cli
 gem install heroku foreman
+
+source ../.osx
